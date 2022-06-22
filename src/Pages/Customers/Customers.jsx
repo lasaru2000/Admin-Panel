f71e1c8cae9a5acd6d4d0c94ad43a9135b211cc9
lasaru2000 <lasaru.2019719@iit.ac.lk>
2022-06-22 17:34:25 +0530

    customer page

---
+++ src/Pages/Customers/Customers.jsx
@@ -0,0 +1,18 @@
+import "./customers.css";
+import Customer from "../../Components/Customers/Customer";
+
+import React from 'react'
+
+export default function customer() {
+    return (
+        <div className ="home">
+
+          <Customer/>
+          <Customer/>
+          <Customer/>
+          
+        </div>
+    )
+}
+
+
