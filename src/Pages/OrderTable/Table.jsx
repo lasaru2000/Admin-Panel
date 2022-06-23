0e11e5b691e195520ba9b9e0fbe31e84fbb22683
lasaru2000 <lasaru.2019719@iit.ac.lk>
2022-06-23 18:14:05 +0530

    ordertable

---
+++ src/Pages/OrderTable/Table.jsx
@@ -0,0 +1,17 @@
+import "./table.css";
+import Table from "../../Components/orderTable/OrderTable";
+
+import React from 'react'
+
+export default function menu() {
+    return (
+        <div className ="home">
+
+            <br></br>
+
+           <Table/>
+           
+            
+        </div>
+    )
+}
