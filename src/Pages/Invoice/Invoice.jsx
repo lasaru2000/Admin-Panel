3abe41bbca00db0ba974565935973f79bea68086
lasaru2000 <lasaru.2019719@iit.ac.lk>
2022-06-24 18:21:13 +0530

    invoice

---
+++ src/Pages/Invoice/Invoice.jsx
@@ -0,0 +1,29 @@
+import "./invoice.css"
+import Invoice1 from "../../Components/Invoice/Invoice";
+
+import React from 'react'
+
+export default function Invoice () {
+    return (
+        <div className ="home">
+
+            <h1 classname="name" >Recent Invoices</h1>
+            
+
+          <Invoice1/>
+          <Invoice1/>
+
+          <Invoice1/>
+
+          <Invoice1/>
+
+          <Invoice1/>
+
+
+         
+            
+        </div>
+    )
+}
+
+
