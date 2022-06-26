abafe34f2e49b7d54587a996f8aea2130f725229
lasaru2000 <lasaru.2019719@iit.ac.lk>
2022-06-26 18:06:36 +0530

    email

---
+++ src/Pages/Email/Email.jsx
@@ -0,0 +1,20 @@
+import "./email.css";
+import Email from "../../Components/Emails/Email";
+
+import React from 'react'
+
+export default function menu() {
+    return (
+        <div className ="home">
+
+
+            <Email/>
+            <Email/>
+            <Email/>
+            <Email/>
+            
+        </div>
+    )
+}
+
+
