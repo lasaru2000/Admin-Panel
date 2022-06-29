88d28f20e5975e3bfb58d669050b789b860f5aee
lasaru2000 <lasaru.2019719@iit.ac.lk>
2022-06-29 18:04:26 +0530

    menu

---
+++ src/Pages/Menus/menu.jsx
@@ -0,0 +1,20 @@
+import "./Menu.css";
+import PhotoList from "../../Components/photoList/photoList";
+
+import React from 'react'
+
+export default function menu() {
+    return (
+        <div className ="home">
+
+            <br></br>
+
+            <PhotoList/>
+            <PhotoList/>
+            <PhotoList/>
+            
+        </div>
+    )
+}
+
+
