00a63e3d34d3d91966f590162ba27fe30d6ceec0
lasaru2000 <lasaru.2019719@iit.ac.lk>
2022-07-15 18:36:26 +0530

    RList

---
+++ src/Pages/Rlist/RList.jsx
@@ -0,0 +1,16 @@
+import "./rList.css";
+import RLIst from "../../Components/RestList/RestList";
+
+import React from 'react'
+
+export default function RList() {
+    return (
+        <div className ="home">
+
+
+           <RLIst/>
+           <RLIst/>
+            
+        </div>
+    )
+}
